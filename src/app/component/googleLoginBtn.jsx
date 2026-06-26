7c058f1749563c91ebea093d131e62820472300c
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2026-06-26 19:20:55 +0600

    google aid

---
+++ src/app/component/googleLoginBtn.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { useAuth } from "../../lib/AppContext";
+
+export default function GoogleLoginBtn() {
+   const {  isLoggedIn } = useAuth();
+
+  const handleSuccess = async (credentialResponse) => {
+    const idToken = credentialResponse.credential;
+
+    try {
+      const response = await fetch("http://localhost:3001/api/auth/google", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ token: idToken }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        login(data.user);
+        alert(`Welcome back, ${data.user.name}!`);
+      } else {
+        alert(data.error || "Authentication failed");
+      }
+    } catch (error) {
+      console.error("Auth Error:", error);
+    }
+  };
+
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  console.log("clientID", clientId);
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <div className="flex justify-center p-4">
+        <GoogleLogin
+          onSuccess={handleSuccess}
+          onError={() => console.log("Login Failed")}
+          useOneTap
+          theme="outline"
+          shape="circle"
+        />
+      </div>
+    </GoogleOAuthProvider>
+  );
+}
